147c02fb3acb7e0fd346ccdb5b2897d0ea7b21b4
sathurshana <50675615+sathurshana@users.noreply.github.com>
2020-04-18 13:14:14 +0530

    Hospital committed

---
+++ hospitalPaf/src/main/java/hospitalPaf/hospitalPaf/Hospital.java
@@ -0,0 +1,108 @@
+package hospitalPaf.hospitalPaf;
+
+
+public class Hospital {
+
+	public int id;
+
+	private String telNo;
+	private String hospitalName;
+	private String hospitalLocation;
+
+	
+//getters and setters
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+
+
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	public String getTelNo() {
+		return telNo;
+	}
+
+
+
+
+
+
+
+	public void setTelNo(String telNo) {
+		this.telNo = telNo;
+	}
+
+
+
+
+
+
+
+	public String getHospitalName() {
+		return hospitalName;
+	}
+
+
+
+
+
+
+
+	public void setHospitalName(String hospitalName) {
+		this.hospitalName = hospitalName;
+	}
+
+
+
+
+
+
+
+	public String getHospitalLocation() {
+		return hospitalLocation;
+	}
+
+
+
+
+
+
+
+	public void setHospitalLocation(String hospitalLocation) {
+		this.hospitalLocation = hospitalLocation;
+	}
+
+
+
+
+//tostring methd
+
+
+	@Override
+	public String toString() {
+		return "Hospital [telNo=" + telNo + ", hospitalName=" + hospitalName
+				+ ", hospitalLocation=" + hospitalLocation + ",  ID=" + id + "]";
+	}
+	
+	
+}
